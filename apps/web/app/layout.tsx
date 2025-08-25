cfb203c318d9de85e3f38ff0d84d91b69026ef26
Marvin Zhang <tikazyq@163.com>
2025-08-25 21:10:42 +0800

    feat: implement middleware for pathname and URL headers; update layout to conditionally render AppLayout

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from 'next';
 import { AppProviders } from '@/components/provider/app-providers';
 import '@/styles/globals.css';
 import '@/styles/fonts.css';
+import { AppLayout } from '@/components/layout/app-layout';
+import { headers } from 'next/headers';
 
 export const metadata: Metadata = {
   title: 'Devlog Management',
@@ -12,10 +14,16 @@ export const metadata: Metadata = {
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
+  const headersList = headers();
+  const pathname = headersList.get('x-pathname') || '';
+  console.log('pathname:', pathname);
+
   return (
     <html lang="en" suppressHydrationWarning>
       <body className="font-inter">
-        <AppProviders>{children}</AppProviders>
+        <AppProviders>
+          {pathname.match(/^\/(login|register)/) ? children : <AppLayout>{children}</AppLayout>}
+        </AppProviders>
       </body>
     </html>
   );
--- apps/web/components/provider/app-providers.tsx
+++ apps/web/components/provider/app-providers.tsx
@@ -1,7 +1,6 @@
 'use client';
 
 import { ThemeProvider } from '@/components/provider/theme-provider';
-import { AppLayout } from '@/components/layout/app-layout';
 import { StoreProvider } from '@/components/provider/store-provider';
 import { AuthProvider } from '@/components/auth';
 
@@ -9,9 +8,7 @@ export function AppProviders({ children }: { children: React.ReactNode }) {
   return (
     <ThemeProvider>
       <AuthProvider>
-        <StoreProvider>
-          <AppLayout>{children}</AppLayout>
-        </StoreProvider>
+        <StoreProvider>{children}</StoreProvider>
       </AuthProvider>
     </ThemeProvider>
   );
